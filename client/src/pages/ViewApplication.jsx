909664be9235bf6d087f774d1069294f78b00699
Dhiraj <royd3366@gmail.com>
2025-11-09 21:33:26 +0530

    view application table alignment

--- client/src/pages/ViewApplication.jsx
+++ client/src/pages/ViewApplication.jsx
@@ -51,46 +51,46 @@ const ViewApplication = () => {
   <div className='flex items-center justify-center h-[70vh]'><p className='text-xl sm:text-2xl'>No Applications Available</p></div>) : (
     <div className='container mx-auto p-4'>
       <div>
-        <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'><thead>
-          <tr className='border-b border-gray-200'>
-            <th className='py-2 px-4 text-left max-sm:hidden'>#</th>
-            <th className='py-2 px-4 text-center'>User name</th>
-            <th className='py-2 px-4 text-left max-sm:hidden max-md:hidden'>Job Title</th>
-            <th className='py-2 px-4 text-left max-sm:hidden max-md:hidden'>Location</th>
-            <th className='py-2 px-4 text-center'>Resume</th>
-            <th className='py-2 px-4 text-center'>Action</th>
-          </tr>
-        </thead>
+        <table className='w-full bg-white border border-gray-200 text-sm sm:text-base'>
+          <thead>
+            <tr className='border-b border-gray-200'>
+              <th className='py-3 px-4 text-left '>#</th>
+              <th className='py-3 px-4 text-center'>User name</th>
+              <th className='py-3 px-4 text-left max-sm:hidden max-md:hidden'>Job Title</th>
+              <th className='py-3 px-4 text-left max-sm:hidden max-md:hidden'>Location</th>
+              <th className='py-3 px-4 text-center'>Resume</th>
+              <th className='py-3 px-4 text-center'>Action</th>
+            </tr>
+          </thead>
           <tbody>
             {applicants.filter(item => item.jobId && item.userId).map((applicant, index) => (
-              <tr key={index} className='text-gray-700'>
-                <td className='py-2 px-4 border-b border-gray-200 text-center'>{index + 1}</td>
-                <td className='py-4 sm:py-4.5 lg:py-7.5 px-4 border-b border-gray-200 text-center flex items-center'>
+              <tr key={index} className='text-gray-700 align-middle'>
+                <td className='py-3 px-4 border-b border-gray-200 text-center '>{index + 1}</td>
+                <td className='py-3 px-4 border-b border-gray-200 text-center flex items-center justify-center'>
                   <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.userId.image} alt="" />
-                  <span>{applicant.userId.name}</span>
+                  <span className='text-center max-sm:text-[16px] max-sm:mb-4 max-sm:mt-4'>{applicant.userId.name}</span>
                 </td>
-                <td className='py-2 px-4 border-b border-gray-200  max-sm:hidden max-md:hidden'>{applicant.jobId.title}</td>
-                <td className='py-2 px-4 border-b border-gray-200 max-sm:hidden max-md:hidden'>{applicant.jobId.location}</td>
-                <td className='py-2 px-4 border-b border-gray-200'>
+                <td className='py-3 px-4 border-b  border-gray-200 max-sm:hidden max-md:hidden'>{applicant.jobId.title}</td>
+                <td className='py-3 px-4 border-b border-gray-200 max-sm:hidden max-md:hidden'>{applicant.jobId.location}</td>
+                <td className='py-3 px-4 border-b border-gray-200 text-center'>
                   <a href={applicant.userId.resume} target='_blank' className='bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center'>
                     Resume <img src={assets.resume_download_icon} alt="" />
                   </a>
                 </td>
-                <td className='py-4 px-4 sm:py-3 md:py-6 border-b border-gray-200 relative flex justify-center items-center'>
-                  {applicant.status === 'Pending' ? <div className='relative inline-block text-center group'>
-                    <button className='text-gray-500 action-button cursor-pointer'>...</button>
-                    <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border
-border-gray-200 rounded shadow group-hover:block '>
-                      <button onClick={() => changeJobApplicationStatus(applicant._id, 'Accepted')} className='block w-full text-center px-4 py-2  text-blue-500
-hover:bg-gray-100 cursor-pointer'>Accept</button>
-                      <button onClick={() => changeJobApplicationStatus(applicant._id, 'Rejected')} className='block w-full text-center px-4 py-2  text-red-500
-hover:bg-gray-100 cursor-pointer'>Reject</button>
+                <td className='py-3 px-4 border-b border-gray-200 text-center'>
+                  {applicant.status === 'Pending' ? (
+                    <div className='relative inline-block text-center group'>
+                      <button className='text-gray-500 action-button cursor-pointer'>...</button>
+                      <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block'>
+                        <button onClick={() => changeJobApplicationStatus(applicant._id, 'Accepted')} className='block w-full text-center px-4 py-2 text-blue-500 hover:bg-gray-100 cursor-pointer'>Accept</button>
+                        <button onClick={() => changeJobApplicationStatus(applicant._id, 'Rejected')} className='block w-full text-center px-4 py-2 text-red-500 hover:bg-gray-100 cursor-pointer'>Reject</button>
+                      </div>
                     </div>
-                  </div> :
-                    <div>
+                  ) : (
+                    <div className='text-center font-medium'>
                       {applicant.status}
-                    </div>}
-
+                    </div>
+                  )}
                 </td>
               </tr>
             ))}
